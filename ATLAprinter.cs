9d3dfa86126ebb71f4976905a4541809db9c7f18
Patchrik <pklly92@gmail.com>
2020-11-22 12:55:25 -0600

    Finished the Title Screen

--- ATLAprinter.cs
+++ ATLAprinter.cs
@@ -13,7 +13,7 @@ namespace ATLAprinter
       {
         Console.Write(text.Substring(0, 1));
         text = text.Remove(0, 1);
-        Thread.Sleep(85);
+        Thread.Sleep(70);
       }
     }
     static void writelines5()
@@ -26,7 +26,7 @@ namespace ATLAprinter
     }
     static void SleepAndClear()
     {
-      Thread.Sleep(1450);
+      Thread.Sleep(1300);
       Console.Clear();
     }
     static void WaterPrinter()
@@ -53,6 +53,12 @@ namespace ATLAprinter
       air.printASCIIart();
       air.ASCIItext();
     }
+    static void TitlePrinter()
+    {
+      Title title = new Title();
+      title.printASCIIart();
+      title.ASCIItext();
+    }
     static void Main(string[] args)
     {
       string intro = "My grandmother used to tell me stories about the old days, a time of peace when the Avatar kept balance between the Water Tribes, Earth Kingdom, Fire Nation, and Air Nomads. But that all changed when the Fire Nation attacked. Only the Avatar mastered all four elements. Only he could stop the ruthless firebenders. But when the world needed him most, he vanished. A hundred years have passed and the Fire Nation is nearing victory in the War. Two years ago, my father and the men of my tribe journeyed to the Earth Kingdom to help fight against the Fire Nation, leaving me and my brother to look after our tribe. Some people believe that the Avatar was never reborn into the Air Nomads, and that the cycle is broken. But I haven't lost hope. I still believe that somehow, the Avatar will return to save the world.";
@@ -67,6 +73,8 @@ namespace ATLAprinter
       AirPrinter();
       SleepAndClear();
       Typer(intro);
+      SleepAndClear();
+      TitlePrinter();
     }
   }
 }
---
+++ Title.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ATLAprinter
+{
+  class Title
+  {
+    public void printASCIIart()
+    {
+      Console.WriteLine(@"ddxxxxxxxxxkkkkkkkkOkdc;'.';lkKXXXXXNNNK0XWWWWWWWWW0KWWWWMMMMMWWWWWWMWNXNWMWWMWWWWWWWWWWMMMMMWWWWWWWNXK0KXNNNWWWWWWMWWWMMMWWWWWWWWWNNNNXXK0000000OOOkkkkkkkxxxxxxxxxxxdxxdddddddddddddddddoooo
+ddxxxxxxxxxkkkkkkkOOl.      ..ckKXXXNNNWWWWWWWWWWWWWWMMWWMMMMMMWWMWWWMMWWWMMMWWMWWWWWWMWMMNK000OOxoc;'...',;;;:loxkO0KNWWMMWWWWWWWWWWNNXXXKKK0000OOOOOkkkkkxxxxxxxxxxxxxxdddddddddddddddddoooo
+dxxxxxxxxxkkkkkkkkkd,          'dKXNNNWWWWKO0XNWWWWWMMWWWWMWWMMWX0KNMMMWWMMMWMMWWNNWWWWWWNd,.....                 ...';o0NWMWWWWWWWWWNNXXXXK0koodkOOkxxddooddxxxxxxxxxxxxxddddddddddddddddoooo
+xxxxxxxxxxkkkkkkkkd:.           'xXXNNNWWNd..':xKNWWWMWWWWMMWWNx,.'oXWMWWMMWWMW0l;:coOXWWKc                           ,o0NWXOdoloOXNWNNXXXK0o.  .,c:'........',;codxxxxxxxxxddddddddddddddoooo
+xxxxxxxxxxkkkkkkko,             .:OXNNNNWXl.   .'lONMWWWMMWMWXd.   .cKWWWMMMMXd'     .c0WNx'                 'codxkxxkKWWNO:.    .c0NNXNNXKd.          ..,;:c:'. .,ldxxxxxxxxddddddddddddddooo
+xxxxxxxxkkxkkkkkl.               .oXNNNWWNx.      .dNWWWWMWW0c.     .xWMMWWWKl.       .:0WWKxooodxxc.       'ONWMWWWWWWWXd'        ;ONNNNXKl.        .cdxxkkxo;.  .;dxxxxxxxxxdddddddddddddooo
+xxxxxxxxxxkkkkd:.       .'..      'xXNNWWWNo.      .oXWWWWNx,      .lKWWWWNk,.    .    .cKWMWWWWWWWXl.     .lXWMMMWWWWXx;.          :ONNXXKo.       'dOOkxol,.   .:oxxxxxxxxxxxxddddddddddddoo
+dxxxxxxxxxkkxl,.       ,dOx;       ,kNWWWWWXd'      .oXWWKl.    .:d0NWMWWXo.   .,dx:.   .cXWWWWWMWMWk'     .dNMMMMWMWO;.      ..     ;OXXXKo.       'llc;..  ..,cdxxxxxxxxxxxxdddddddddddddddo
+ddddxxxxxxxo,.        .lkOOo.       ,xKNWWWWNk;      .oKk;.   .:ONWWWWWNO:.   .oKWW0:    .lKWWMMMMMW0:     .xNMMWWWNk,      'o0k,    .c0XXKl.        .   ..;codxxxxxxxxxxxxxxddddddddddddddddd
+dxdooool:,..           ...',.        .':dOXNWW0;      .'.    .oK0xooool:.     .lkOxo;.    .c0WMMMMMWKc.    .kWMWWX0o'      ,xXKOc.    .l0X0l.          .:oxkkkkkkkxxxxxxxxxxxxdddddddddddddddd
+l:,.....                                 .,cxKNO;           ;kNXl.              ..         .,kNWWWMMKc.    'kWMW0:.        .'''..      .lKXx'          .:dkkkkkkxxxkkxxxxxxxxxxddddddddddddddd
+.                                           ,xNWO;         ;0WWNd.              .......      ,kNWWMMK:     'OWMNx.                      ,OXKc.           .,ldxkkkxxkkxxxxxxxxxxxdddddddddddddd
+                                           .c0WWNx.      .:0WWMWNOc'      .,cloxkO0K000x;.    :kKWMMXc.    'OWMW0;      ..',,::cc:.     .lXKo.   ':,.      ...;ldxxkxxxxxxxxxxdxdddddddddddddd
+.                            ...          .lKWWMW0:     .lKWMWWMWWNd.   .l0XWWWWMMMMMMMMKc.   ,oOWMWNd.    'OWMWk,     'd0XXNNWWWNk'     cKKd.  .l0Oxl,.       ..,;codxxxxxxxxxxxddddddddddddd
+l,.               ..',;:clodxkkkd,.       cXWMWWMNx,...,dXWWMMMMMM0,  .;kNWWWWWWMMMWMMWWWKo'.;d0XWWWNx.    ,OWMKc.    .oXWWMMWMMMMNx'    :KKd'  .o0O0Okd;.          ...,:cloodddddddddddddoodd
+xdlc,.         .,cdxOO00KKKKXXXXXO:.      :KWMWWMWNX0O0XWMMMMMWWMMKl:lkXWMMMWMWMMWMMWMWWMWNKKXWWWWWMW0l.  .cKWMXo.  .;dKWWWMMWMMMWWNk:...lKXx'.'ck0OOOOOkdl:,.            ......'''''''''''.',
+xxxxl.       .;dkOO000000KKKXXXXXXd.      ,OWMWWMMMMWWMMWMMMMMMMMWWWWWMMMMMMMWWMMMMWMMMMMWWWMMMWWMMWWNOc. ,0WWWWN0xx0NWWMMWMMMMMMWWWWX0kOKXXkclk00OO0OOOOOOOkdl;.                           ..
+xxxxd;...  .'lxOOkOOOO000KKKKKXXXXKl.     :KWMMWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWKl. lNMWMMWMMMMMMMMMMMMWMMMWWWWNNNXXKK0kO00OO0OOOOOOOOkOOkdl:'..                 ...';co
+xxxxxxdollldxkkkkkOOOO00000KKKKKXXXKd,..,l0WWMWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXd;cKWMMMMMMMMMMMMMMMMMMMWWWWWWNNXXXXKK00000OOOOOOOOOOOkkkkkkkdolc::::;;;;;;::cccllodddd
+xxxxxxkkkkkkkkkkkkOOOOO0000KKKKKKXXXX0O0XNWWWWWWMMMMWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWMMMWX0XWMMMWWMMMMMMMWMWWWWWWWWWWWWNXXXKKK0000000O0OOOOOOOOkkkkkkkkkkkkkkkkxxxxxxxxxxddxdddx");
+    }
+    public void ASCIItext()
+    {
+      Console.WriteLine(@"
+.___________. __    __   _______     __          ___           _______.___________.        ___       __  .______      .______    _______ .__   __.  _______   _______ .______      
+|           ||  |  |  | |   ____|   |  |        /   \         /       |           |       /   \     |  | |   _  \     |   _  \  |   ____||  \ |  | |       \ |   ____||   _  \     
+`---|  |----`|  |__|  | |  |__      |  |       /  ^  \       |   (----`---|  |----`      /  ^  \    |  | |  |_)  |    |  |_)  | |  |__   |   \|  | |  .--.  ||  |__   |  |_)  |    
+    |  |     |   __   | |   __|     |  |      /  /_\  \       \   \       |  |          /  /_\  \   |  | |      /     |   _  <  |   __|  |  . `  | |  |  |  ||   __|  |      /     
+    |  |     |  |  |  | |  |____    |  `----./  _____  \  .----)   |      |  |         /  _____  \  |  | |  |\  \----.|  |_)  | |  |____ |  |\   | |  '--'  ||  |____ |  |\  \----.
+    |__|     |__|  |__| |_______|   |_______/__/     \__\ |_______/       |__|        /__/     \__\ |__| | _| `._____||______/  |_______||__| \__| |_______/ |_______|| _| `._____|
+                                                                                                                                                                                   
+");
+    }
+  }
+}
